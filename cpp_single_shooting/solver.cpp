5077999977c7b9a15a1b45c54f43429d3fe24a8b
alwaki <xalwa999@gmail.com>
2022-10-02 09:49:51 +0200

    Changed main to run. Also changed to pass param struct!

--- cpp_single_shooting/solver.cpp
+++ cpp_single_shooting/solver.cpp
@@ -1,23 +1,24 @@
 #include "solver.h"
-
-std::pair <double, double> Tensor_fit(double x, double F, double G, const containers::parameters& params)
+/*
+std::pair <double, double> Tensor_fit(double x, double F, double G, double sigmaR, double sigmaV0, 
+	double sigmaa, double dR, double dV0, double da, double tensorV, const containers::parameters& params)
 {
 	double Col = 0;
 
 	if (params.isospin == 1)
 	{
-		if (x > params.sigmaR)
+		if (x > sigmaR)
 		{
 			Col = 0.0072923 * params.Z / x;
 		}
 		else
 		{
-			Col = 0.0072923 * params.Z * (3 * pow(params.sigmaR, 2) - pow(x, 2)) / (2 * pow(params.sigmaR, 3));
+			Col = 0.0072923 * params.Z * (3 * pow(sigmaR, 2) - pow(x, 2)) / (2 * pow(sigmaR, 3));
 		}
 	}
-	double sigma = params.sigmaV0 / (1 + exp((x - params.sigmaR) / params.sigmaa)) + Col;
-	double delta = params.dV0 / (1 + exp((x - params.dR) / params.da)) + Col;
-	double U = params.tensorV / (1 + exp((x - params.sigmaR) / params.sigmaa));
+	double sigma = sigmaV0 / (1 + exp((x - sigmaR) / sigmaa)) + Col;
+	double delta = dV0 / (1 + exp((x - dR) / da)) + Col;
+	double U = tensorV / (1 + exp((x - sigmaR) / sigmaa));
 
 	double dfgW_1 = (-params.B + sigma) * G + (params.k / x - U) * F;
 	double dfgW_2 = (2 * params.m + params.B - delta) * F + (U - params.k / x) * G;
@@ -25,7 +26,8 @@ std::pair <double, double> Tensor_fit(double x, double F, double G, const contai
 }
 
 std::pair <double, double> int_n_Tensor(double xstart, double xend, double iniF, double iniG,
-	const containers::parameters& params)
+	double sigmaR, double sigmaV0, double sigmaa, double dR, double dV0, double da, 
+	double tensorV, const containers::parameters& params)
 {
 	double h = 0.001;
 	double step = (xend - xstart) * h;
@@ -42,10 +44,13 @@ std::pair <double, double> int_n_Tensor(double xstart, double xend, double iniF,
 	for (int i = 0; i++; i < end_condition)
 	{
 		x = i * step + xstart;
-		v1 = Tensor_fit(x, F, G, params);
-		v2 = Tensor_fit(x + step_2, F + v1.first * step_2, G + v1.second * step_2, params);
-		v3 = Tensor_fit(x + step_2, F + v2.first * step_2, G + v2.second * step_2, params);
-		v4 = Tensor_fit(x + step, F + v3.first * step, G + v3.second * step, params);
+		v1 = Tensor_fit(x, F, G, sigmaR, sigmaV0, sigmaa, dR, dV0, da, tensorV, params);
+		v2 = Tensor_fit(x + step_2, F + v1.first * step_2, G + v1.second * step_2, 
+			sigmaR, sigmaV0, sigmaa, dR, dV0, da, tensorV, params);
+		v3 = Tensor_fit(x + step_2, F + v2.first * step_2, G + v2.second * step_2, 
+			sigmaR, sigmaV0, sigmaa, dR, dV0, da, tensorV, params);
+		v4 = Tensor_fit(x + step, F + v3.first * step, G + v3.second * step, 
+			sigmaR, sigmaV0, sigmaa, dR, dV0, da, tensorV, params);
 		F += (v1.first + 2 * v2.first + 2 * v3.first + v4.first) * step_6;
 		G += (v1.second + 2 * v2.second + 2 * v3.second + v4.second) * step_6;
 	}
@@ -141,13 +146,13 @@ std::tuple<double, double, double, double> BC_pos(double xmin, double xmax, cons
 
 	return { Foutbc, Goutbc, Finbc / norm, Ginbc / norm };
 }
-
+*/
 /* RETURN TO GAP FUNCTION LATER
 double Gap(x, y) :
 	return np.array(x - y).T
 
 */
-
+/*
 bool test_converge(double B, double Exp)
 {
 	if (Exp < 0)
@@ -171,14 +176,10 @@ void solve_dirac()
 {
 
 }
-
+*/
 int read_user_input(std::string type)
 {
 	int temp;
-	std::cout << "The program requires a scenario and state. These \n";
-	std::cout << "are both integers, and the scenario ranges from 1 to 3 \n";
-	std::cout << "while the state ranges from 1 to 89. \n";
-	std::cout << "\n";
 	std::cout << "Enter " << type << ": ";
 	std::cin >> temp;
 	if (type == "scenario")
@@ -218,25 +219,103 @@ int read_user_input(std::string type)
 	return temp;
 }
 
-std::vector<double> read_row(const string file_name, int row)
-{
-	string line;
-	fstream fin;
-	fin.open(file_name, ios::in);
-	getline(fin, line);
+std::vector<std::vector<double>> read_csv(std::string filename) {
+	// Reads a CSV file into a vector of <string, vector<int>> pairs where
+	// each pair represents <column name, column values>
 
+	// Create an input filestream
+	std::ifstream myFile(filename);
+
+	// Make sure the file is open
+	if (!myFile.is_open()) throw std::runtime_error("Could not open file");
+
+	std::vector<std::vector<double>> content;
+	std::vector<double> row;
+	std::string line, word;
+
+	while (getline(myFile, line))
+	{
+		row.clear();
+
+		std::stringstream str(line);
+
+		while (getline(str, word, ','))
+		{
+			try
+			{
+				row.push_back(std::stod(word));
+			}
+			catch (...)
+			{
+
+			}
+		}
+		content.push_back(row);
+	}
+	/*
+	for (int i = 0; i < content.size(); i++)
+	{
+		for (int j = 0; j < content[i].size(); j++)
+		{
+			std::cout << content[i][j] << " ";
+		}
+		std::cout << "\n";
+	} */
+
+	return content;
 }
 
 int main()
 {
+	// Allow user to select scenario and state
+	std::cout << "The program requires a scenario and state. These \n";
+	std::cout << "are both integers, and the scenario ranges from 1 to 3 \n";
+	std::cout << "while the state ranges from 1 to 89. \n";
+	std::cout << "\n";
 	int scenario = read_user_input("scenario");
 	int state = read_user_input("state");
 
+	// Load data from files
+	auto data = read_csv("data.csv");
+	auto parameters = read_csv("parameters.csv");
+
+	// Set parameters dependent on scenario
+	double V0 = parameters[scenario][1];
+	double kappa = parameters[scenario][2];
+	double lambda = parameters[scenario][3];
+	double r0 = parameters[scenario][4];
+	double a = parameters[scenario][5];
+	double Rls = parameters[scenario][6];
+	double als = parameters[scenario][7];
+
+	// Set parameters dependent on state
+	int isospin = data[state][2];
+	double N = data[state][3];
+	double Z = data[state][4];
+	double l = data[state][5];
+	double k = data[state][6];
+	double B = data[state][7];
+	double xmin = data[state][8];
+	double xmax = data[state][9];
+	double xmatch = data[state][10];
+	double a0 = data[state][11];
+
+	// Set other parameters
+	double tensorV = 0;
+	double m = 0;
+	if (isospin == -1)
+		m = NEUTRON_MASS_MEV;
+	else
+		m = PROTON_MASS_MEV;
+
+	// Create parameter struct
+	containers::parameters params(V0, kappa, lambda, r0, a, Rls, als, isospin, N, Z, l, k, xmin, xmax,
+		xmatch, m, scenario, tensorV);
+	
 
 
 
 
 
-	std::cout << "hi";
 	return 0;
 }
--- cpp_single_shooting/solver.h
+++ cpp_single_shooting/solver.h
@@ -9,6 +9,8 @@
 #include <iostream>
 #include <fstream>
 #include <limits>
+#include <utility> // std::pair
+#include <stdexcept> // std::runtime_error
 
 
 #define NEUTRON_MASS_MEV 939.56542052
@@ -16,22 +18,52 @@
 
 namespace containers
 {
-	typedef struct
+	typedef struct parameters
 	{
-		double a0;
-		double B;
-		double sigmaV0;
-		double sigmaR;
-		double sigmaa;
-		double dV0;
-		double dR;
-		double da;
-		double k;
-		double m;
-		double Z;
-		double tensorV;
-		int isospin;
-		int l;
+    public:
+        double V0 = 0;
+        double kappa = 0;
+        double lambda = 0;
+        double r0 = 0;
+        double a = 0;
+        double Rls = 0;
+        double als = 0;
+        double isospin = 0;
+        double N = 0;
+        double Z = 0;
+        double l = 0;
+        double k = 0;
+        double xmin = 0;
+        double xmax = 0;
+        double xmatch = 0;
+        double m = 0;
+        int scenario = 0;
+        double tensorV = 0;
+
+        parameters(double v0, double v1, double v2, double v3, 
+            double v4, double v5, double v6, double v7, double v8,
+            double v9, double v10, double v11, double v12, double v13, 
+            double v14, double v15, int v16, double v17)
+        {
+            double V0 = v0;
+            double kappa = v1;
+            double lambda = v2;
+            double r0 = v3;
+            double a = v4;
+            double Rls = v5;
+            double als = v6;
+            double isospin = v7;
+            double N = v8;
+            double Z = v9;
+            double l = v10;
+            double k = v11;
+            double xmin = v12;
+            double xmax = v13;
+            double xmatch = v14;
+            double m = v15;
+            int scenario = v16;
+            double tensorV = v17;
+        }
 
 	} parameters;
 }
