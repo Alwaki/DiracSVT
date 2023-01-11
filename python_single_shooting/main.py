ce939f998517d717263e7c0bd55b467fed22c038
Alexander <59147412+Alwaki@users.noreply.github.com>
2023-01-11 16:01:28 +0100

    finalize main.py

--- python_single_shooting/main.py
+++ python_single_shooting/main.py
@@ -1,12 +1,9 @@
 from util import *
 from shooting_method import *
 
+# Set state according to datasheet
 state = ['208Pb 3p1/2', -1]
-stuff = load_data("data.xlsx", "parameters.xlsx")
-data = stuff[0]
-for i in range(0, 5):
-    state = [data['Name'][i], -1]
-    B, a0 = run_1Dsolve(state, Scenario=1)
-    print(B)
-    #print(a0)
-    #print("B: ", B, " a0: ", a0)
+
+# Solver is run
+B, a0 = run_1Dsolve(state, Scenario=1)
+print("B: ", B, " a0: ", a0)
