0cfc892c4c953c98d3a8139598fdd4db89dbc31a
Alexander <59147412+Alwaki@users.noreply.github.com>
2023-02-11 16:44:17 +0100

    extended pardf to get all values

--- python_single_shooting/util.py
+++ python_single_shooting/util.py
@@ -51,9 +51,9 @@ def load_data(filename1, filename2):
     pardf= pd.read_excel(os.getcwd()+"\\python_single_shooting\\"+filename2)
     data.index = data['Unnamed: 0']
     data.pop('Unnamed: 0')
-    p1 = {pardf.columns[x]:pardf[pardf.Scenario == 1].values[0][x] for x in range(1,8)}
-    p2 = {pardf.columns[x]:pardf[pardf.Scenario == 2].values[0][x] for x in range(1,7)}
-    p3 = {pardf.columns[x]:pardf[pardf.Scenario == 3].values[0][x] for x in range(1,8)}
+    p1 = {pardf.columns[x]:pardf[pardf.Scenario == 1].values[0][x] for x in range(1,9)}
+    p2 = {pardf.columns[x]:pardf[pardf.Scenario == 2].values[0][x] for x in range(1,8)}
+    p3 = {pardf.columns[x]:pardf[pardf.Scenario == 3].values[0][x] for x in range(1,9)}
 
     meq = "mass energy equivalent in MeV"
     mn = constants.physical_constants["neutron " + meq][0] #energy in MeV
